8d4aba9d276b7536b27609da3840175c042a308c
jasonc86 <jasonc86@gmail.com>
2022-08-20 13:27:21 -0400

    modified:   .gitignore 	modified:   dailylotto.py 	modified:   lotto-results-to-SMS.py 	deleted:    random_ua.py 	deleted:    user_agents.json

--- dailylotto.py
+++ dailylotto.py
@@ -1,69 +1,60 @@
 #!/usr/bin/python3
-'''Module for retrieving the NY lottery numbers and Win 4 game results'''
-import time
 import requests
+import time
+import traceback
 import sys
-from random_ua import rand_user_agent
+from twtxt import twilio_text
 
-def request_json(game):
-	'''Request for JSON data from url.'''
-	url = f'https://nylottery.ny.gov/nyl-api/games/{game}/draws'
-	headers = {'User-agent': rand_user_agent()}
-	try:
-		res = requests.get(url, headers=headers, timeout=60)
-		res.raise_for_status()
-	except requests.HTTPError as e:
-		print(f'Status Code: {e.response.status_code}')
-		return False
-	return res.json()
+class dailyLotto:
 
-def result_check(game):
-	'''Checks JSON data to see if results are available for latest game.
-	   if "results" key in draw_data exists it returns draw_data.'''
-	json_data = request_json(game)
-	if json_data:
-		draw_data = json_data['data']['draws'][1]
-		if draw_data.get('results', ''):
-			return draw_data
-	else:
-		return False
+	def __init__(self, game_name):
+		'''initializes attributes for the data I want to send'''
+		self.game_name = game_name
+		self.json_url = f'https://nylottery.ny.gov/nyl-api/games/{game_name}/draws'
+		self.draw_dict = {1: "Midday", 2: "Evening"}
+		self.json_data = {}
+		self.draw_period = ''
+		self.results = ''
+		# Error message if the program fails to get desired data
+		err_url = f"https://nylottery.ny.gov/draw-game?game={self.game_name}."
+		self.err_msg = f'Data unavailable.\nCheck results at\n{err_url}'
+	
+	def send_err(self):
+		'''Sets result to error message'''
+		self.results = self.err_msg
+		
+	def get_data(self):
+		'''requests JSON data from url and returns results'''
+		for x in range(4):
+			try:
+				res = requests.get(self.json_url, timeout=60)
+				self.json_data = res.json()
+				draw_data = self.json_data['data']['draws'][1]
+				self.draw_period = self.draw_dict.get(draw_data['drawPeriod'], '')
+				self.results = ''.join(draw_data['results'][0]['primary'])
+				break
+			except Exception:
+				time.sleep(164) 
+		else:
+			print(traceback.format_exc())
+			self.send_err()
+		return f"{self.game_name.title()}: {self.results}\n"
 
-def json_to_str(draw_data):
-	'''Converts JSON data to string and formats it for readability.
-	   See included JSON files to see how the desired data is structured.'''
-	try:
-		results = draw_data.get('results', '')
-	except AttributeError:
-		return False
-	draw_period = {1: "Midday", 2: "Evening"}
-	numbers = ''.join(results[0]['primary'])
-	result_date = time.strftime("%x", time.localtime((draw_data['resultDate']/1000)))
-	results_info = (draw_period[draw_data['drawPeriod']],
-		draw_data['gameName'].title(), result_date, numbers)
-	return "{0[1]}: {0[3]}".format(results_info), "{0[0]} {0[2]}".format(results_info)
+	def get_draw(self):
+		return f"{self.draw_period} {time.strftime('%x')}"
 
-def format_sms(games, interval, timeout):
-	'''Checks to see if results for each game is available. If it isn't it waits and then checks the results for the other game.'''
-	start = time.time()
-	while not all([v for v in games.values()]):
-		for game in games.keys():
-			draw_data = result_check(game)
-			if draw_data and not games[game]:
-				games[game] = json_to_str(draw_data)
-			else:
-				time.sleep(interval)
-		if time.time() - start > timeout:
-			for k, v in games.items():
-				url = f"https://nylottery.ny.gov/draw-game?game={k}."
-				if v == False:
-					games[k] = f"Request failed. Retrieve results at {url}"
-			break
+def sms_from_data():
+	msg = ''
+	games = ('numbers', 'win4')
+	for game in games:
+		dl = dailyLotto(game)
+		msg += dl.get_data()
+	msg += dl.get_draw()
+	return msg
 
 def main():
-	games = {'numbers':False, 'win4':False}
-	format_sms(games, 30, 3600)
-	print('\n'.join([v[0] for v in games.values()]+[v[1] for v in games.values()][1:]))
-	#print([v for v in games.values()])
+	msg = sms_from_data()
+	print(msg)
 
 if __name__ == '__main__':
 	main()
--- lotto-results-to-SMS.py
+++ lotto-results-to-SMS.py
@@ -7,12 +7,8 @@ def main():
 	'''Uses twilio to send lotto information.
 	This was separated from dailylotto.py so I could test changes to that file
 	without sending text messages.'''
-	games = {'numbers':False, 'win4':False}
-	format_sms(games, 30, 3600)
-	msg = [v[0] for v in games.values()] # Contains winning numbers for each game
-	msg += [v[1] for v in games.values()][1:] # Shows draw period and date for results
-	#msg += [v[2] for v in games.values()][1:] # Shows draw period and date for results
-	twilio_text('\n'.join(msg))
+	msg = sms_from_data()
+	twilio_text(msg)
 
 if __name__ == '__main__':
 	main()
